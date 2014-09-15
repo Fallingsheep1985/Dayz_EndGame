4e6ff58d3d3fd64f12d3db37fe0348c7270dfb72
Fallingsheep1985 <lee@integr8it.com.au>
2014-09-15 11:08:24 +1000

    Mechs!!!

---
+++ MPMissions/DayZ_EndGame_1.Napf/CfgServerTrader/Category/EndGameMechs.hpp
@@ -0,0 +1,12 @@
+class Category_5000 {
+		class Alien_dis9ver5 {
+		type = "trade_any_vehicle";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class districtmech{
+		type = "trade_any_vehicle";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+};
