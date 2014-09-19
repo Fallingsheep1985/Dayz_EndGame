88831e9598115d597cdb6edb111de801f855a295
fallingsheep <razorsoftinfo@gmail.com>
2014-09-20 08:33:37 +1000

    new skins trader
    added new skins to trader

--- MPMissions/DayZ_EndGame_1.Napf/CfgServerTrader/Category/EndGameClothing.hpp
+++ MPMissions/DayZ_EndGame_1.Napf/CfgServerTrader/Category/EndGameClothing.hpp
@@ -19,4 +19,150 @@ class Category_4003 {
 		buy[] = {2,"ItemGoldBar"};
 		sell[] = {1,"ItemGoldBar"};
 	};
+			//Press
+		class Skin_civil_pressmanikf {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_civil_pressmanikf2 {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_civil_pressmanik {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};;
+		};
+		class Skin_civil_pressmanik2 {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		//GSC STALKER
+		class Skin_gsc_military_helmet_wdl  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_gsc_military_helmet_grey  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_gsc_military_head_wdl  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_gsc_scientist1  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_gsc_scientist2  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_gsc_eco_stalker_mask_fred  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_gsc_eco_stalker_mask_camo  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_gsc_cloth_loner_head  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_gsc_eco_stalker_mask_duty  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_gsc_eco_stalker_mask_neutral  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		//SWAT
+		class Skin_SWAT  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		//KPFS CIvilians
+		class Skin_KPFS_Postman_west  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_KPFS_Postman_west_cap  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_KPFS_Postman_dpag  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		//SRA Police
+		class Skin_SRA_RACS_Policeman  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_SRA_RACS_Police_Elite  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_SRA_RACS_Police_Elite_B  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_SRA_RACS_Police_Elite_Sniper  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		//SRA Milica
+		class Skin_SRA_SLA_Policeman  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_SRA_SLA_Policeman_vest  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_SRA_SLA_Police_Elite  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_SRA_SLA_Police_Elite_Officer  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_SRA_SLA_Police_Elite_Sniper  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
+		class Skin_SRA_SLA_Police_Crew  {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+		};
 };
